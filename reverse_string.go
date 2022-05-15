7d62b2fa9bfd0ed4d278eb1928e21ff55e6e0301
Mariyash1086 <milly10031986@gmail.com>
2022-05-15 21:45:34 +0400

    its HW 4-1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,17 @@
 package reverse_string
 
+//package main
+
 func ReverseString(input string) (output string) {
 	// solution goes here
+
+	result := []string{}
+	for i := len(input) - 1; i >= 0; i-- {
+		result = append(result, string(input[i]))
+	}
+
+	for _, j := range result {
+		output = output + " " + j
+	}
 	return output
 }
